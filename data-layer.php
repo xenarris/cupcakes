060ed0a307211661b3c5b35ac93b80fea05ecf19
Mark Mendoza <mmendoza-rabara@mail.greenriver.edu>
2021-05-13 14:12:55 -0400

    getFlavors returns value

--- data-layer.php
+++ data-layer.php
@@ -4,4 +4,5 @@ function getFlavors()
 {
     $result = array("grasshopper"=>"The Grasshopper", "maple"=>"Whiskey Maple Bacon", "carrot"=>"Carrot Walnut",
         "caramel"=>"Salted Caramel Cupcake", "velvet"=>"Red Velvet", "lemon"=>"Lemon Drop", "tiramisu"=>"Tiramisu");
+    return $result;
 }
